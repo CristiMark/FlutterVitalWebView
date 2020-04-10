01c88f824cf30231fdca95de61d200e7bc1981d5
Cristian Keisz <cristian.keisz@universalalloy.com>
2020-04-10 13:04:22 +0300

    am mai facut cateva modificari

--- lib/main.dart
+++ lib/main.dart
@@ -48,8 +48,12 @@ class RunJSInWebViewState extends State<RunJSInWebView> {
   void initState(){
     super.initState();
     loadJS();
+    flutterWebviewPlugin.onUrlChanged.listen((String url) {
+      if (mounted) {
+        print("Current URL: $url");
+      }
+    });
     
-
   }
 
 
@@ -67,7 +71,7 @@ class RunJSInWebViewState extends State<RunJSInWebView> {
         flutterWebviewPlugin.evalJavascript(js);  
       }
     });
-    flutterWebviewPlugin.reloadUrl("https://plati.vitalmm.ro/plata.jsf;jsessionid=944C833F0538EF0DC6766C59ACFC137B");
+    flutterWebviewPlugin.reloadUrl("https://plati.vitalmm.ro/welcome.jsf;jsessionid=4FC7C694CEB8D9FAD5B9D6037A089005?Adf-Window-Id=w0");
     return;
   });
 }
@@ -78,145 +82,9 @@ class RunJSInWebViewState extends State<RunJSInWebView> {
       url: 'https://plati.vitalmm.ro/login.jsp',
      // hidden: true,
      withZoom: true,
-      appBar: AppBar(title: Text("MyVital")),
+      appBar: AppBar(title: Text("My Vital Acount")),
       
     );
   }
 }
 
-// class HomePage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text("Home Page"),       
-//       ),
-//       body: Center(
-//         child: FlatButton(
-//           child: Text("Open Vital"),
-//           onPressed:(){
-//            Navigator.of(context).push(MaterialPageRoute(
-//         builder: (BuildContext context) => MyWebView(
-//               title: "MyVital",
-//               selectedUrl: "https://plati.vitalmm.ro/login.jsp",
-//             )));
-//           },
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// class MyWebView extends StatelessWidget {
-//   final String title;
-//   final String selectedUrl;
-
-//   final Completer<WebViewController> _controller =
-//       Completer<WebViewController>();
-      
-//   MyWebView({
-//     @required this.title,
-//     @required this.selectedUrl,
-//   });
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         appBar: AppBar(
-//           title: Text(title),
-//         ),
-//         body: WebView(
-//           initialUrl: selectedUrl,
-//           javascriptMode: JavascriptMode.unrestricted,
-//           onWebViewCreated: (WebViewController webViewController) {
-//             _controller.complete(webViewController);
-//           },
-//         ));
-//   }
-// }
-
-// class MyHomePage extends StatefulWidget {
-//   MyHomePage({Key key, this.title}) : super(key: key);
-
-//   // This widget is the home page of your application. It is stateful, meaning
-//   // that it has a State object (defined below) that contains fields that affect
-//   // how it looks.
-
-//   // This class is the configuration for the state. It holds the values (in this
-//   // case the title) provided by the parent (in this case the App widget) and
-//   // used by the build method of the State. Fields in a Widget subclass are
-//   // always marked "final".
-
-//   final String title;
-
-//   @override
-//   _MyHomePageState createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       // This call to setState tells the Flutter framework that something has
-//       // changed in this State, which causes it to rerun the build method below
-//       // so that the display can reflect the updated values. If we changed
-//       // _counter without calling setState(), then the build method would not be
-//       // called again, and so nothing would appear to happen.
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // This method is rerun every time setState is called, for instance as done
-//     // by the _incrementCounter method above.
-//     //
-//     // The Flutter framework has been optimized to make rerunning build methods
-//     // fast, so that you can just rebuild anything that needs updating rather
-//     // than having to individually change instances of widgets.
-//     return Scaffold(
-//       appBar: AppBar(
-//         // Here we take the value from the MyHomePage object that was created by
-//         // the App.build method, and use it to set our appbar title.
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         // Center is a layout widget. It takes a single child and positions it
-//         // in the middle of the parent.
-//         child: Column(
-//           // Column is also a layout widget. It takes a list of children and
-//           // arranges them vertically. By default, it sizes itself to fit its
-//           // children horizontally, and tries to be as tall as its parent.
-//           //
-//           // Invoke "debug painting" (press "p" in the console, choose the
-//           // "Toggle Debug Paint" action from the Flutter Inspector in Android
-//           // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-//           // to see the wireframe for each widget.
-//           //
-//           // Column has various properties to control how it sizes itself and
-//           // how it positions its children. Here we use mainAxisAlignment to
-//           // center the children vertically; the main axis here is the vertical
-//           // axis because Columns are vertical (the cross axis would be
-//           // horizontal).
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headline4,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         backgroundColor: Colors.blue,
-//         child: Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
